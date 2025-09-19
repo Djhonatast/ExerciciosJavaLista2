289bd81897659877d8451ee331918d376a8acf62
Djhonatas Teixeira <djhonatas100505@edu.unipar.br>
2025-09-18 22:25:02 -0300

    Add files via upload

---
+++ ExLista2_4.java
@@ -0,0 +1,11 @@
+//Tendo como dados de entrada a altura, o peso e o sexo de uma pessoa, construa um algoritmo que calcule seu peso ideal, usando as fórmulas abaixo e exiba se a pessoa está abaixo, no peso ou acima do peso ideal.
+	//homens: (72.7 * H) - 58
+	//mulheres: (62.1 * H) - 44.7
+import java.util.Scanner;
+class ExLista2_4{
+Scanner s = new Scanner(System.in);
+double altura, peso;
+String sexo;
+
+
+}
