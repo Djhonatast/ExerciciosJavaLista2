97b64a44007c121dddd86a7550248a7dd5e3f646
Djhonatas Teixeira <djhonatas100505@edu.unipar.br>
2025-09-18 22:22:53 -0300

    Corrigido

--- Exlista2_1.java
+++ Exlista2_1.java
@@ -6,8 +6,9 @@ class Exlista2_1{
     double volume;
     System.out.print("Insira o Raio para calcular o volume da esfera: ");
     raio = s.nextDouble();
-    volume = 4/3 * Math.PI * Math.pow(raio,3);
-    System.out.print("O volume da esfera é de " + volume);
+    //volume = (4 * Math.PI * Math.pow(raio,3))/3;
+    volume = 4.0 / 3.0 * Math.PI * Math.pow(raio,3);
+    System.out.printf("O volume da esfera é de %4.4f", volume);
 
 
 
