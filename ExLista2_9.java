421fca1f864ec7d0ab2e80433fefeea6c4a3dd62
Djhonatas Teixeira <djhonatas100505@edu.unipar.br>
2025-09-25 21:20:17 -0300

    Add files via upload

---
+++ ExLista2_9.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+class ExLista2_9{
+    public static void main(String[] args) {
+Scanner s = new Scanner(System.in);
+double valortotal, novovaltot;
+int codigo;
+System.out.print("Insira o valor total da compra: ");
+valortotal = s.nextDouble();
+System.out.print(" Código1 - A vista com 10% de desconto ");
+System.out.print("Código 2 A prazo, 1x com 8% de desconto ");
+System.out.print("Código 3 A prazo, 5x sem desconto ou acréscimos ");
+System.out.print("Código 4 A prazo, 10x com acréscimo  de 5% no valor total. ");
+System.out.print("Digite o código da opção escolhida: ");
+    }
+}
+
